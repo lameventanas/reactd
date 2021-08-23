2585b788ea3cb1e8a40e37949c540607b60b4187
Alan <lameventanas@users.noreply.github.com>
2021-08-23 17:24:09 +0900

    keylist: - added debug code - improve keylist_set()

--- keylist.h
+++ keylist.h
@@ -1,34 +1,26 @@
 #ifndef KEYLIST_H
 #define KEYLIST_H
 
-/*
- * keylist library: single linked list using a string as key
- * list is sorted alphabetically by the key
- */
-
 typedef struct _keylist {
     char *key;
     void *value;
     struct _keylist *next;
 } keylist;
 
-/*
- * Adds item
- * If key already exists, replaces value (and returns previous one)
- * If key doesn't exist, returns NULL
- * key is strdupped
- */
 void *keylist_set(keylist **list, char *key, void *value);
 
-/*
- * Returns value, or NULL if not present
- */
 void *keylist_get(keylist **list, char *key);
 
-/*
- * Removes and returns value, or NULL if not present
- * key is freed
- */
-void *keylist_del(keylist **list, char *key);
+void keylist_del_match(keylist **list, int (*callback)(void *, void *), void *args);
+
+void keylist_free(keylist **list, void (*callback)(void *));
+
+unsigned int keylist_count(keylist **list);
+
+#ifdef DEBUG_KEYLIST
+
+// print debug information, callback is optional
+void keylist_print(keylist **list, void (*callback)(void *));
+#endif
 
 #endif
